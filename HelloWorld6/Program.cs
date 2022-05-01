72022c453368fa452475e0c39865fce8c7394fd8
ROXANA lazarescu <roxana_lazarescu13@yahoo.com>
2022-05-01 16:19:46 +0300

    Add project files.

---
+++ HelloWorld6/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace HelloWorld6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a");
+            int a = 5;
+            Console.WriteLine("b");
+            int b = 6;
+            Console.WriteLine("c");
+            int c = 7;
+            if (a > b)
+            {
+                if (c > a)
+                {
+                    Console.WriteLine(c);
+                    Console.WriteLine(a);
+                    Console(WriteLine(b);
+                } else if (b > c)
+                {
+                    Console.WriteLine(a);
+                    Console.WriteLine(b);
+                    Console.WriteLine(c);
+
+                }
+                else
+                {
+                    Console.WriteLine(a);
+                    Console.WriteLine(c);
+                    Console.WrtiteLine(b);
+                }
+            }
+            else
+            {
+                if (c > b)
+                {
+                    Console.WriteLine(c);
+                    Console.WriteLine(b);
+                    Console.WriteLine(a);
+                }
+
+                else if(a>c)
+                {
+                    Console.WriteLine(b);
+                    Console.WriteLine(a);
+                    Console.WriteLine(c);
+
+                }
+                else
+                {
+                    Console.WriteLine(b);
+                    Console.WriteLine(c);
+                    Console.WriteLine(a);
+                }
+                    
+                
+            }
+        }
+    }
+}
